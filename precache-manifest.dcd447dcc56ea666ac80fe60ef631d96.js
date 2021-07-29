c2e97ac3e8974546fdf8c7381eb2d9ae379a10b8
Code Sandbox <hello@codesandbox.io>
2021-07-29 13:56:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dcd447dcc56ea666ac80fe60ef631d96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e893a2f154b875645ba0c1f3fed5cee",
+    "url": "https://chircalescode.github.io/csb-xw5gc/index.html"
+  },
+  {
+    "revision": "65c0766ee7aec0d96911",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/css/2.c09586da.chunk.css"
+  },
+  {
+    "revision": "459df9bb8d01231b9bfd",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/css/main.1a030b5a.chunk.css"
+  },
+  {
+    "revision": "65c0766ee7aec0d96911",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/js/2.6062710f.chunk.js"
+  },
+  {
+    "revision": "459df9bb8d01231b9bfd",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/js/main.41d60fd9.chunk.js"
+  },
+  {
+    "revision": "d6caaa994a896305579b",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/js/runtime~main.1d279575.js"
+  },
+  {
+    "revision": "4519aea339b63bce6e1868865586f405",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/media/dxicons.4519aea3.woff2"
+  },
+  {
+    "revision": "f1829bb1cb996a760a819b4eb23b6108",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/media/dxicons.f1829bb1.ttf"
+  },
+  {
+    "revision": "fd4164a956b105e4ec9f6c12de2d6b82",
+    "url": "https://chircalescode.github.io/csb-xw5gc/static/media/dxicons.fd4164a9.woff"
+  }
+]);
